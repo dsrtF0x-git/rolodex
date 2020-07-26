3ab6920e47d83a96c3a4d77f0dade711ba2351fc
Serhii <gespenst.serge@yandex.ru>
2020-07-26 09:54:38 +0300

    added search field

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // import logo from './logo.svg'
-import './App.css';
-import CardList from './components/card-list/CardList';
+import "./App.css";
+import CardList from "./components/card-list/CardList";
 
 function App() {
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => setMonsters(users));
   }, []);
 
   const [monsters, setMonsters] = useState([]);
-
-  // const filteredMonsters =
+  const [searchField, setSearchField] = useState("");
 
   return (
     <div className="App">
+      <input
+        type="search"
+        placeholder="search monsters"
+        onChange={(e) => setSearchField(e.target.value)}
+      />
       <CardList monsters={monsters} />
     </div>
   );
