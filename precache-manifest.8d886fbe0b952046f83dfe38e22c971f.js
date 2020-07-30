45a7a1d22638fc3b54df526e744d2a282d66984f
Serhii <gespenst.serge@yandex.ru>
2020-07-30 10:29:57 +0300

    Updates

---
+++ precache-manifest.8d886fbe0b952046f83dfe38e22c971f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f99efc6253a24c89d6f8baf7402940b",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "4012ace09afaa2933e79",
+    "url": "/rolodex/static/css/main.53e70080.chunk.css"
+  },
+  {
+    "revision": "43cd1e4b3cd0a6e923d0",
+    "url": "/rolodex/static/js/2.51b5c0f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rolodex/static/js/2.51b5c0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4012ace09afaa2933e79",
+    "url": "/rolodex/static/js/main.f5b15a23.chunk.js"
+  },
+  {
+    "revision": "ce484491ce0eb150302f",
+    "url": "/rolodex/static/js/runtime-main.867464ce.js"
+  }
+]);
